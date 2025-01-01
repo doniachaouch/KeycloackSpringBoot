c878b62ac7b115cc5e5db40e07b8f6c43f446cbc
CHAOUCH Donia <dchaouch01@gmail.com>
2025-01-01 12:49:57 +0100

    test and debug

--- src/main/java/edu/polytech/keycloak/SecurityConfig.java
+++ src/main/java/edu/polytech/keycloak/SecurityConfig.java
@@ -1,22 +1,28 @@
 package edu.polytech.keycloak;
 
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
-    private  final JwtAuthConverter jwtAuthConverter;
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Autowired
+    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -25,15 +31,16 @@ public class SecurityConfig {
                 .authorizeHttpRequests()
                 .anyRequest()
                 .authenticated();
+
         http
                 .oauth2ResourceServer()
                 .jwt()
                 .jwtAuthenticationConverter(jwtAuthConverter);
+
         http
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionCreationPolicy(STATELESS);
 
         return http.build();
-
     }
 }
